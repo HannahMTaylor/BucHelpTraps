259ab207d5c6248a4a8d32d38af2f31e9fa53951
Ethan Morgan <Morganer@etsu.edu>
2023-02-13 16:04:45 -0500

    Created Feature branch for submitting questions

--- BucHelp/Program.cs
+++ BucHelp/Program.cs
@@ -1,4 +1,5 @@
 using BucHelp.Data;
+using Syncfusion.Blazor;
 using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Web;
 
@@ -8,6 +9,7 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
 builder.Services.AddSingleton<WeatherForecastService>();
+builder.Services.AddSyncfusionBlazor();
 
 var app = builder.Build();
 
