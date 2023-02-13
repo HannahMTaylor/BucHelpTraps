cd8abc401cb8a64918639ee841ffcae076b7ac03
HannahMTaylor <61762557+HannahMTaylor@users.noreply.github.com>
2023-02-13 17:10:12 -0500

    Added a data file for the Question Object -- do not have a service page designed for it yet -- empty home page for Posted Questions has been made, not functional yet

---
+++ BucHelp/Data/Question.cs
@@ -0,0 +1,14 @@
+﻿namespace BucHelp.Data
+{
+    public class Question
+    {
+        public int QuestionID { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string UserName { get; set; }
+        public DateTime Created { get; set; }
+        public DateTime LastUpdated { get; set; }
+
+
+    }
+}
