0833d02438be0af5223df3efe42511cab63c14e0
Ethan Morgan <Morganer@etsu.edu>
2023-02-14 15:12:41 -0500

    Added logic to automatically create username based off email given.  Fixed a NavLink bug for posthome

--- BucHelp/Data/User.cs
+++ BucHelp/Data/User.cs
@@ -2,12 +2,29 @@
 {
     public class User
     {
-        public string UserName { get; set; }
-
+        public string UserName { get; set; } //Will be the email
         public string Password { get; set; }
-
         public string Email { get; set; }
+        public string Affiliation { get; set; } //Student or Faculty
 
-        public string Affiliation { get; set; }
+        public void setEmail(string email)
+        {
+            Email = email;
+            //Set the username here as well
+            string username = email.Substring(0, email.IndexOf("@")); //Removed the @xxx.xxx
+            UserName = username;
+        }
+
+        public User(string password, string email, string affiliation)
+        {
+            Password = password;
+            setEmail(email);
+            Affiliation = affiliation;
+        }
+
+        public User()
+        {
+
+        }
     }
 }
