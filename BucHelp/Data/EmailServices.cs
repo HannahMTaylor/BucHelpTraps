1020495b3e1f9d9c3582c00d7ccc72baae14674b
Ethan Morgan <Morganer@etsu.edu>
2023-02-16 15:48:39 -0500

    Added EmailServices

---
+++ BucHelp/Data/EmailServices.cs
@@ -0,0 +1,38 @@
+﻿using System.Net;
+using System.Net.Mail;
+namespace BucHelp.Data
+{
+    public class EmailServices
+    {
+        SmtpClient Client;
+
+        public EmailServices()
+        {
+            Client = new SmtpClient
+            {
+                Host = "smtp.gmail.com",
+                Port = 587,
+                EnableSsl = true,
+                DeliveryMethod = SmtpDeliveryMethod.Network,
+                UseDefaultCredentials = false,
+                Credentials = new NetworkCredential("Trapezoidsinc@gmail.com", "#TRAP!n") 
+            };
+        }
+
+        public void SendEmail(string toEmail, string body, string subject)
+        {
+            MailAddress from = new MailAddress("Trapezoidsinc@gmail.com", "BucHelp - Trapeziods", System.Text.Encoding.UTF8);
+            MailAddress to = new MailAddress(toEmail);
+
+            MailMessage message = new MailMessage(from, to);
+
+            message.Body = body;
+            message.BodyEncoding = System.Text.Encoding.UTF8;
+            message.Subject = subject;
+            message.SubjectEncoding = System.Text.Encoding.UTF8;
+
+            Client.Send(message);
+
+        }
+    }
+}
