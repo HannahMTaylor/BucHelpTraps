50fe3a9989b42f4449677f1b53bdcb976c986427
ZergRushNoooo <92892716+ZergRushNoooo@users.noreply.github.com>
2023-02-19 01:57:26 -0500

    Login Update
    Login Page tab
    Email text box
    Password text box
    Log In button

---
+++ BucHelp/Data/Login.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel.DataAnnotations;
+using System.Net;
+
+namespace BucHelp.Data
+{
+    public class LoginModel : PageModel
+    {
+        [BindProperty]
+        public Credential Credential { get; set; }
+        public void OnGet()
+        {
+
+        }
+        
+        public void OnPost() 
+        {
+
+        }
+    }
+
+    public class Credential
+    {
+        [Required]
+        public string Email { get; set; }
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+    }
+}
