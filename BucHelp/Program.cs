0177a7d09200e07feb2e2fd83111a99f72341b31
Ethan Morgan <Morganer@etsu.edu>
2023-02-20 12:41:59 -0500

    Added persistent local storage for questions.

--- BucHelp/Program.cs
+++ BucHelp/Program.cs
@@ -1,6 +1,7 @@
 using BucHelp.Data;
 using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Web;
+using Microsoft.Extensions.DependencyInjection.Extensions;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -8,7 +9,7 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
 builder.Services.AddSingleton<WeatherForecastService>();
-builder.Services.AddSingleton<Question>();
+builder.Services.TryAddScoped<Question>();
 
 var app = builder.Build();
 
