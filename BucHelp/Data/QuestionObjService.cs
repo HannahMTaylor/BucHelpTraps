667cc8ce198a7ba99ddd00355d6dc140cc358ac0
HannahMTaylor <61762557+HannahMTaylor@users.noreply.github.com>
2023-02-20 16:08:29 -0500

    Add ability to display the hardcoded list of question

--- BucHelp/Data/QuestionObjService.cs
+++ BucHelp/Data/QuestionObjService.cs
@@ -3,24 +3,20 @@
     public class QuestionObjService
     {
         //temporary list of static questions
-        Question[] questions = new Question[4];
+        List<Question> questions = new List<Question>();
 
-        Question q1 = new Question { QuestionID = 0, Title = "title1", Description = "Where is graduation normally held", Answer = "In the Mini-Dome"};
-        Question q2 = new Question { QuestionID = 1, Title = "title1", Description = "How do I apply to graduate?", Answer = "in Goldlink" };
-        
-        
-
-        
+        Question q1 = new Question { QuestionID = 0, Title = "Question 1", Description = "Where is graduation normally held?", Answer = "In the Mini-Dome"};
+        Question q2 = new Question { QuestionID = 1, Title = "Quesiont 2", Description = "How do I apply to graduate?", Answer = "in Goldlink" };
 
 
-        //need a method to get the data
-        public Question[] GetPostedQuestions()
+
+
+
+        public Task<List<Question>> GetPostedQuestionsAsync()
         {
-            questions[0] = q1;
-            questions[1] = q2;
-
-            //add code here to interface with Liam's db service
-            return questions;
+            questions.Add(q1);
+            questions.Add(q2);
+            return Task.FromResult(questions);
         }
     }
 }
