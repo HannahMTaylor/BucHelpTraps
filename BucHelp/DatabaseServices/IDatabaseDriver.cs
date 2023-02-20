3ebed6b84a88179238c4db5b5159a6ca8deee090
lwhitelaw <48133276+lwhitelaw@users.noreply.github.com>
2023-02-20 15:43:10 -0500

    add create table to interface

--- BucHelp/DatabaseServices/IDatabaseDriver.cs
+++ BucHelp/DatabaseServices/IDatabaseDriver.cs
@@ -15,6 +15,16 @@
         public ITable GetTableForName(string name);
 
         /// <summary>
+        /// Create a table with the specified name and header. Fails with null if the table exists already.
+        /// </summary>
+        /// <param name="name">table name</param>
+        /// <param name="header">table header</param>
+        /// <returns>
+        ///     a handle to the specified table
+        /// </returns>
+        public ITable CreateTable(string name, RowHeader header);
+
+        /// <summary>
         /// Commit any unsaved changes to the database, if the implementation does not do so automatically.
         /// </summary>
         public void Commit();
