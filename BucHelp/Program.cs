9c4c20ea546529e94641c3c29e92153a6ffe0082
HannahMTaylor <61762557+HannahMTaylor@users.noreply.github.com>
2023-02-22 15:34:41 -0500

    Update Program.cs
    removed truncated weather services call from program.cs

--- BucHelp/Program.cs
+++ BucHelp/Program.cs
@@ -8,7 +8,7 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
-builder.Services.AddSingleton<WeatherForecastService>();
+
 builder.Services.TryAddScoped<Question>();
 
 var app = builder.Build();
