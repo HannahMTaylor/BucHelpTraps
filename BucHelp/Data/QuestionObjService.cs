5c77b7a58de0d3681b177d202d5bd45ad3148b93
lwhitelaw <48133276+lwhitelaw@users.noreply.github.com>
2023-03-01 13:14:23 -0500

    Deserialisation for questions

--- BucHelp/Data/QuestionObjService.cs
+++ BucHelp/Data/QuestionObjService.cs
@@ -1,4 +1,6 @@
-﻿namespace BucHelp.Data
+﻿using BucHelp.DatabaseServices;
+
+namespace BucHelp.Data
 {
     public class QuestionObjService
     {
@@ -18,6 +20,50 @@
             questions.Add(q1);
             questions.Add(q2);
             return Task.FromResult(questions);
+        }
+
+        // return question for ID
+        public static Question GetForId(int qid)
+        {
+            IDatabaseDriver driver = Drivers.GetDefaultDriver();
+            IEnumerator<Row> enumerator = driver.GetTableForName("question").Select(row => row.GetAsInt("id") == qid).GetEnumerator();
+            if (enumerator.MoveNext())
+            {
+                Row row = enumerator.Current;
+                return FromRow(row);
+            }
+            else
+            {
+                return null; // not found
+            }
+        }
+
+        // return all
+        public static List<Question> GetAll()
+        {
+            IDatabaseDriver driver = Drivers.GetDefaultDriver();
+            IEnumerator<Row> enumerator = driver.GetTableForName("question").SelectAll().GetEnumerator();
+            List<Question> questions = new List<Question>();
+            while (enumerator.MoveNext())
+            {
+                Row row = enumerator.Current;
+                questions.Add(FromRow(row));
+            }
+            return questions;
+        }
+
+        // row -> obj conversion
+        private static Question FromRow(Row row)
+        {
+            Question question = new Question();
+            question.QuestionID = row.GetAsInt("id");
+            question.Title = row.GetAsString("title");
+            question.Description = row.GetAsString("description");
+            question.UserName = row.GetAsString("username");
+            question.Answer = row.GetAsString("answer");
+            question.Created = DateTime.Parse(row.GetAsString("created"));
+            question.LastUpdated = DateTime.Parse(row.GetAsString("lastupdated"));
+            return question;
         }
     }
 }
