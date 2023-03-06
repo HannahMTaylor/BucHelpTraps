e83302aea8b9c302aa58a0b087e69861538083b7
lwhitelaw <48133276+lwhitelaw@users.noreply.github.com>
2023-03-06 11:10:56 -0500

    nullability handling

--- BucHelp/Data/QuestionObjService.cs
+++ BucHelp/Data/QuestionObjService.cs
@@ -64,7 +64,24 @@ namespace BucHelp.Data
             return nextId + 1;
         }
 
+        // write row out
+        public static void Write(Question question)
+        {
+            // Remove the row if it is already there and reinsert
+            // not as efficient but the update would be painful to write
+            GetTable().Delete(row => row.GetAsInt("id") == question.QuestionID);
+            GetTable().Insert(ToRow(question));
+        }
+
         // private below here
+        // null bits
+        // a set bit indicates that field is null
+        // 1 = null, 0 = present!
+        private const int NULL_TITLE = 0x01;
+        private const int NULL_DESCRIPTION = 0x02;
+        private const int NULL_USERNAME = 0x04;
+        private const int NULL_ANSWER = 0x08;
+
         // get or create table
         private static ITable GetTable()
         {
@@ -75,10 +92,13 @@ namespace BucHelp.Data
             // table not present, so create it
             RowHeader rowHeader = new RowHeader(
                 new Column("id", Column.Type.Integer),
+                new Column("nulls", Column.Type.Integer),
+                // nullable
                 new Column("title", Column.Type.Text),
                 new Column("description", Column.Type.Text),
                 new Column("username", Column.Type.Text),
                 new Column("answer", Column.Type.Text),
+                // nonnull
                 new Column("created", Column.Type.Text),
                 new Column("lastupdated", Column.Type.Text)
             );
@@ -92,20 +112,49 @@ namespace BucHelp.Data
         {
             Question question = new Question();
             question.QuestionID = row.GetAsInt("id");
-            question.Title = row.GetAsString("title");
-            question.Description = row.GetAsString("description");
-            question.UserName = row.GetAsString("username");
-            question.Answer = row.GetAsString("answer");
+            // get nulls
+            int nulls = row.GetAsInt("nulls");
+            // if null bit is zero, then field is present- get it
+            if ((nulls & NULL_TITLE) == 0) question.Title = row.GetAsString("title");
+            if ((nulls & NULL_DESCRIPTION) == 0) question.Description = row.GetAsString("description");
+            if ((nulls & NULL_USERNAME) == 0) question.UserName = row.GetAsString("username");
+            if ((nulls & NULL_ANSWER) == 0) question.Answer = row.GetAsString("answer");
+            // these are never null
             question.Created = DateTime.Parse(row.GetAsString("created"));
             question.LastUpdated = DateTime.Parse(row.GetAsString("lastupdated"));
             return question;
         }
 
-        // obj -> row conversion
+        // obj -> row conversion, updates do not use this
         private static Row ToRow(Question question)
         {
             Row row = new Row(GetTable().Header);
+            row.SetAsInt("id", question.QuestionID);
+            // build nulls
+            int nulls = 0;
+            SetAsStringNullable(row, "title", question.Title, NULL_TITLE, ref nulls);
+            SetAsStringNullable(row, "description", question.Description, NULL_DESCRIPTION, ref nulls);
+            SetAsStringNullable(row, "username", question.UserName, NULL_USERNAME, ref nulls);
+            SetAsStringNullable(row, "answer", question.Answer, NULL_ANSWER, ref nulls);
+            row.SetAsString("created", question.Created.ToString());
+            row.SetAsString("lastupdated", question.LastUpdated.ToString());
+            row.SetAsInt("nulls", nulls);
             return row;
+        }
+
+        // set row nullable string
+        private static void SetAsStringNullable(Row row, string column, string value, int mask, ref int nulls)
+        {
+            if (value != null)
+            {
+                row.SetAsString(column, value);
+            }
+            else
+            {
+                // placeholder string- never read, but required anyway
+                row.SetAsString(column, "<null>");
+                nulls |= mask;
+            }
         }
     }
 }
