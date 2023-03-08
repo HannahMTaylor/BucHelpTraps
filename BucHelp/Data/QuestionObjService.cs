db802a42f7d3ba3f0539210dcb6215da4a6b8935
HannahMTaylor <61762557+HannahMTaylor@users.noreply.github.com>
2023-03-07 19:29:21 -0500

    Combine the code from search bar and list of posted questions into one page

--- BucHelp/Data/QuestionObjService.cs
+++ BucHelp/Data/QuestionObjService.cs
@@ -8,13 +8,36 @@
         //hardcode attributes for the temp list
         Question q1 = new Question { QuestionID = 0, Title = "Question 1", Description = "Where is graduation normally held?", Answer = "In the Mini-Dome"};
         Question q2 = new Question { QuestionID = 1, Title = "Question 2", Description = "How do I apply to graduate?", Answer = "in Goldlink" };
-
+        Question q11 = new Question { QuestionID = 0, Title = "Question 1", Description = "Where is graduation normally held?", Answer = "In the Mini-Dome" };
+        Question q12 = new Question { QuestionID = 1, Title = "Question 2", Description = "How do I apply to graduate?", Answer = "in Goldlink" };
+        Question q111 = new Question { QuestionID = 0, Title = "Question 1", Description = "Where is graduation normally held?", Answer = "In the Mini-Dome" };
+        Question q112 = new Question { QuestionID = 1, Title = "Question 2", Description = "How do I apply to graduate?", Answer = "in Goldlink" };
+        Question q1111 = new Question { QuestionID = 0, Title = "Question 1", Description = "Where is graduation normally held?", Answer = "In the Mini-Dome" };
+        Question q1112 = new Question { QuestionID = 1, Title = "Question 2", Description = "How do I apply to graduate?", Answer = "in Goldlink" };
+        Question q11111 = new Question { QuestionID = 0, Title = "Question 1", Description = "Where is graduation normally held?", Answer = "In the Mini-Dome" };
+        Question q11112 = new Question { QuestionID = 1, Title = "Question 2", Description = "How do I apply to graduate?", Answer = "in Goldlink" };
+        Question q21 = new Question { QuestionID = 0, Title = "Question 1", Description = "Where is graduation normally held?", Answer = "In the Mini-Dome" };
+        Question q22 = new Question { QuestionID = 1, Title = "Question 2", Description = "How do I apply to graduate?", Answer = "in Goldlink" };
+        Question q221 = new Question { QuestionID = 0, Title = "Question 1", Description = "Where is graduation normally held?", Answer = "In the Mini-Dome" };
+        Question q222 = new Question { QuestionID = 1, Title = "Question 2", Description = "How do I apply to graduate?", Answer = "in Goldlink" };
 
 
 
         //method to retrieve posted questions -- should be able to access the persistent storage as is
         public Task<List<Question>> GetPostedQuestionsAsync()
         {
+            questions.Add(q1);
+            questions.Add(q2);
+            questions.Add(q1);
+            questions.Add(q2);
+            questions.Add(q1);
+            questions.Add(q2);
+            questions.Add(q1);
+            questions.Add(q2);
+            questions.Add(q1);
+            questions.Add(q2);
+            questions.Add(q1);
+            questions.Add(q2);
             questions.Add(q1);
             questions.Add(q2);
             return Task.FromResult(questions);
