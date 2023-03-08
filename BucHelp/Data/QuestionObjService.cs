c11aa9e3d9d421cd86312ac1346099f9aae481c7
HannahMTaylor <61762557+HannahMTaylor@users.noreply.github.com>
2023-03-08 10:44:34 -0500

    fix run issues from sprint 2 merge conflicts -- object persistance to CSV is not working in Dev as of yet. but all other functionality seems to be fixed

--- BucHelp/Data/QuestionObjService.cs
+++ BucHelp/Data/QuestionObjService.cs
@@ -167,57 +167,57 @@ namespace BucHelp.Data
 
 
 
-<!-- Keeping code from combinationPostSearch just in case
-﻿namespace BucHelp.Data
-{
-    public class QuestionObjService
-    {
-        //temporary list of static questions
-        List<Question> questions = new List<Question>();
+//Keeping code from combinationPostSearch just in case
+//﻿namespace BucHelp.Data
+//{
+//    public class QuestionObjService
+//    {
+//        //temporary list of static questions
+//        List<Question> questions = new List<Question>();
 
-        //hardcode attributes for the temp list
-        Question q1 = new Question { QuestionID = 0, Title = "Question 1", Description = "Where is graduation normally held?", Answer = "In the Mini-Dome"};
-        Question q2 = new Question { QuestionID = 1, Title = "Question 2", Description = "How do I apply to graduate?", Answer = "in Goldlink" };
-        Question q11 = new Question { QuestionID = 0, Title = "Question 1", Description = "Where is graduation normally held?", Answer = "In the Mini-Dome" };
-        Question q12 = new Question { QuestionID = 1, Title = "Question 2", Description = "How do I apply to graduate?", Answer = "in Goldlink" };
-        Question q111 = new Question { QuestionID = 0, Title = "Question 1", Description = "Where is graduation normally held?", Answer = "In the Mini-Dome" };
-        Question q112 = new Question { QuestionID = 1, Title = "Question 2", Description = "How do I apply to graduate?", Answer = "in Goldlink" };
-        Question q1111 = new Question { QuestionID = 0, Title = "Question 1", Description = "Where is graduation normally held?", Answer = "In the Mini-Dome" };
-        Question q1112 = new Question { QuestionID = 1, Title = "Question 2", Description = "How do I apply to graduate?", Answer = "in Goldlink" };
-        Question q11111 = new Question { QuestionID = 0, Title = "Question 1", Description = "Where is graduation normally held?", Answer = "In the Mini-Dome" };
-        Question q11112 = new Question { QuestionID = 1, Title = "Question 2", Description = "How do I apply to graduate?", Answer = "in Goldlink" };
-        Question q21 = new Question { QuestionID = 0, Title = "Question 1", Description = "Where is graduation normally held?", Answer = "In the Mini-Dome" };
-        Question q22 = new Question { QuestionID = 1, Title = "Question 2", Description = "How do I apply to graduate?", Answer = "in Goldlink" };
-        Question q221 = new Question { QuestionID = 0, Title = "Question 1", Description = "Where is graduation normally held?", Answer = "In the Mini-Dome" };
-        Question q222 = new Question { QuestionID = 1, Title = "Question 2", Description = "How do I apply to graduate?", Answer = "in Goldlink" };
+//        //hardcode attributes for the temp list
+//        Question q1 = new Question { QuestionID = 0, Title = "Question 1", Description = "Where is graduation normally held?", Answer = "In the Mini-Dome"};
+//        Question q2 = new Question { QuestionID = 1, Title = "Question 2", Description = "How do I apply to graduate?", Answer = "in Goldlink" };
+//        Question q11 = new Question { QuestionID = 0, Title = "Question 1", Description = "Where is graduation normally held?", Answer = "In the Mini-Dome" };
+//        Question q12 = new Question { QuestionID = 1, Title = "Question 2", Description = "How do I apply to graduate?", Answer = "in Goldlink" };
+//        Question q111 = new Question { QuestionID = 0, Title = "Question 1", Description = "Where is graduation normally held?", Answer = "In the Mini-Dome" };
+//        Question q112 = new Question { QuestionID = 1, Title = "Question 2", Description = "How do I apply to graduate?", Answer = "in Goldlink" };
+//        Question q1111 = new Question { QuestionID = 0, Title = "Question 1", Description = "Where is graduation normally held?", Answer = "In the Mini-Dome" };
+//        Question q1112 = new Question { QuestionID = 1, Title = "Question 2", Description = "How do I apply to graduate?", Answer = "in Goldlink" };
+//        Question q11111 = new Question { QuestionID = 0, Title = "Question 1", Description = "Where is graduation normally held?", Answer = "In the Mini-Dome" };
+//        Question q11112 = new Question { QuestionID = 1, Title = "Question 2", Description = "How do I apply to graduate?", Answer = "in Goldlink" };
+//        Question q21 = new Question { QuestionID = 0, Title = "Question 1", Description = "Where is graduation normally held?", Answer = "In the Mini-Dome" };
+//        Question q22 = new Question { QuestionID = 1, Title = "Question 2", Description = "How do I apply to graduate?", Answer = "in Goldlink" };
+//        Question q221 = new Question { QuestionID = 0, Title = "Question 1", Description = "Where is graduation normally held?", Answer = "In the Mini-Dome" };
+//        Question q222 = new Question { QuestionID = 1, Title = "Question 2", Description = "How do I apply to graduate?", Answer = "in Goldlink" };
 
 
 
-        //method to retrieve posted questions -- should be able to access the persistent storage as is
-        public Task<List<Question>> GetPostedQuestionsAsync()
-        {
+//        //method to retrieve posted questions -- should be able to access the persistent storage as is
+//        public Task<List<Question>> GetPostedQuestionsAsync()
+//        {
 
-            //questions.Add(q1);
-            //questions.Add(q2);
+//            //questions.Add(q1);
+//            //questions.Add(q2);
 
-            questions.Add(q1);
-            questions.Add(q2);
-            questions.Add(q1);
-            questions.Add(q2);
-            questions.Add(q1);
-            questions.Add(q2);
-            questions.Add(q1);
-            questions.Add(q2);
-            questions.Add(q1);
-            questions.Add(q2);
-            questions.Add(q1);
-            questions.Add(q2);
-            questions.Add(q1);
-            questions.Add(q2);
+//            questions.Add(q1);
+//            questions.Add(q2);
+//            questions.Add(q1);
+//            questions.Add(q2);
+//            questions.Add(q1);
+//            questions.Add(q2);
+//            questions.Add(q1);
+//            questions.Add(q2);
+//            questions.Add(q1);
+//            questions.Add(q2);
+//            questions.Add(q1);
+//            questions.Add(q2);
+//            questions.Add(q1);
+//            questions.Add(q2);
 
-            return Task.FromResult(questions);
-        }
-    }
-}
--->
+//            return Task.FromResult(questions);
+//        }
+//    }
+//}
+
 
