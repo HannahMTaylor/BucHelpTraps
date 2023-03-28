f25ee87839d5ff7ff8168bcf8ebd13a73a7a3eba
Ethan Morgan <Morganer@etsu.edu>
2023-03-28 15:43:04 -0400

    Added comments

--- BucHelp/Data/NavUtil.cs
+++ BucHelp/Data/NavUtil.cs
@@ -2,10 +2,18 @@
 
 namespace BucHelp.Data
 {
+    /// <summary>
+    /// Utility class for the Navigation Menu
+    /// </summary>
     public class NavUtil
     {
         public int ID = 0;
 
+        /// <summary>
+        /// Will set the Id of the User and will notify a state change to the navmenu passing this 
+        /// variable to.
+        /// </summary>
+        /// <param name="id"></param>
         public void SetID(int id)
         {
             if(!int.Equals(ID, id))
