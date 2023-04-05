3476e992502185b86fb0088564e0f2fa12aa7f0b
Stephen Maurer <92892716+ZergRushNoooo@users.noreply.github.com>
2023-04-05 16:28:51 -0400

    Updates

---
+++ BucHelp/Data/Vote.cs
@@ -0,0 +1,22 @@
+﻿namespace BucHelp.Data
+{
+    public class Vote
+    {
+        bool vote;
+        string voteType;
+
+        public Vote(bool vote, string voteType)
+        {
+            this.vote = vote;
+            this.voteType = voteType;
+
+            UpdateVote();
+        }
+
+        private void UpdateVote()
+        {
+            //stub
+        }
+
+    }
+}
