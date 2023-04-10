13bdfb8adb1d56dc3f3a25cfe3ed8c08980d7ef2
lwhitelaw <48133276+lwhitelaw@users.noreply.github.com>
2023-04-10 17:40:33 -0400

    start work on clientside

---
+++ BucHelp/DatabaseServices/MicroserviceServices.cs
@@ -0,0 +1,12 @@
+﻿using BucHelp.Data;
+
+namespace BucHelp.DatabaseServices
+{
+    private async Task<FAQ[]> GetFAQ()
+    {
+        using (HttpClient client = new HttpClient())
+        {
+            return await client.GetFromJsonAsync("http://192.168.1.3/getfaq", FAQ);
+        }
+    }
+}
